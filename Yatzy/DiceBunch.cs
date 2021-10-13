d5e4314c391afc7ca6e10c43f2ff0b1ffdb6480e
Bhuvana <bhuvana.maganti@myob.com>
2021-10-13 18:11:03 +1100

    Add ReRoll functionality to DiceBunch

--- Yatzy/DiceBunch.cs
+++ Yatzy/DiceBunch.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Yatzy
 {
     public class DiceBunch
     {
         private const int NumberOfDicePerRoll = 5;
+        private const int MaximumRolls = 3;
         public Die[] Dice { get; private set; }
+        public int Rolls { get; private set; }
 
         public DiceBunch()
         {
@@ -15,8 +21,28 @@ namespace Yatzy
         {
             for (var i = 0; i < NumberOfDicePerRoll; i++)
                 Dice[i] = new Die();
+            Rolls = 1;
         }
-        
 
+        public int[] GetDiceValues()
+        {
+            return Dice.Select(die => die.FaceValue).ToArray();
+        }
+
+        public void ReRoll(int[] diceToReRoll)
+        {
+            if (!CanBeRolled())
+                throw new InvalidOperationException("This dice bunch is rolled maximum times");
+            foreach (var index in diceToReRoll)
+            {
+                Dice[index - 1].Roll();
+            }
+            Rolls++;
+        }
+
+        public bool CanBeRolled()
+        {
+            return Rolls < MaximumRolls;
+        }
     }
 }
--- YatzyTests/UnitTests.cs
+++ YatzyTests/UnitTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 using Yatzy;
 
@@ -62,6 +63,83 @@ namespace YatzyTests
             {
                 Assert.True(die.FaceValue < 7);
             }
+        }
+        
+        // Selected dice can be rerolled
+        [Fact]
+        public void ReRoll_ChangesFaceValueOfSelectedDice()
+        {
+            DiceBunch diceBunch = new DiceBunch();
+            var initialDiceValues = diceBunch.GetDiceValues();
+            var diceToReRoll = new[] {1, 3, 5};
+            diceBunch.ReRoll(diceToReRoll);
+            var reRolledDiceValues = diceBunch.GetDiceValues();
+            Assert.NotEqual(initialDiceValues, reRolledDiceValues);
+        }
+        
+        
+        [Fact]
+        public void ReRoll_DoesNotChangeNonSelectedDiceValues()
+        {
+            // Arrange
+            DiceBunch diceBunch = new DiceBunch();
+            var initialDiceValues = diceBunch.GetDiceValues();
+            var diceToReRoll = new[] {4, 5};
+            // Act
+            diceBunch.ReRoll(diceToReRoll);
+            var reRolledDiceValues = diceBunch.GetDiceValues();
+            // Assert
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.Equal(initialDiceValues[i], reRolledDiceValues[i]);
+            }
+        }
+
+        // Dice can be rolled / re rolled thrice
+        [Fact]
+        public void Rolls_ForANewDiceBunch_Returns1()
+        {
+            // Arrange
+            DiceBunch diceBunch = new DiceBunch();
+            Assert.Equal(1, diceBunch.Rolls);
+        }
+        
+        [Fact]
+        public void Rolls_After1ReRoll_Returns2()
+        {
+            // Arrange
+            DiceBunch diceBunch = new DiceBunch();
+            diceBunch.ReRoll(new int[]{1});
+            Assert.Equal(2, diceBunch.Rolls);
+        }
+        
+        // Maximum Rolls should be handled by Engine?
+        [Fact]
+        public void MoreThan2ReRolls_ThrowsException()
+        {
+            DiceBunch bunch = new DiceBunch();
+            bunch.ReRoll(new int[]{1});
+            bunch.ReRoll(new int[]{1});
+            Assert.Throws<InvalidOperationException>(() => bunch.ReRoll(new int[]{1}));
+        }
+        
+        // Split into 2 tests
+        [Fact]
+        public void CanBeRolled_Before2ReRolls_ReturnsTrue()
+        {
+            DiceBunch bunch = new DiceBunch();
+            Assert.True(bunch.CanBeRolled());
+            bunch.ReRoll(new int[]{1});
+            Assert.True(bunch.CanBeRolled());
+        }
+        
+        [Fact]
+        public void CanBeRolled_After2ReRolls_ReturnsFalse()
+        {
+            DiceBunch bunch = new DiceBunch();
+            bunch.ReRoll(new int[]{1});
+            bunch.ReRoll(new int[]{1});
+            Assert.False(bunch.CanBeRolled());
         }
     }
 }
